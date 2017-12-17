628b1abbf52cd0b608f5f2a5e4c1a9fa940e0d61
Surya T Paruchuri <stparuchuri@gmail.com>
2017-12-16 19:31:42 -0600

    copied a bash conditions, loops tutorial

---
+++ bash/conditions_loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a tutorial describing bash conditions and loops.
+# Author: Surya Tej Paruchuri
+
+$x = 1;
+if ($x ==1)
+	echo "the valueo of x is 1"
+#else
+#	echo "the value of x is not 1"
+fi
+
